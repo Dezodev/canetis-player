10be554974d3a2660c1acf5040bbe9e646f7f34e
Dezodev <dezodev@gmail.com>
2023-08-06 16:50:45 +0200

    Format files

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -7,55 +7,51 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const defaultInclude = path.resolve(__dirname, 'src')
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+                include: defaultInclude,
+            },
+            {
+                test: /\.jsx?$/,
+                use: [{ loader: 'babel-loader' }],
+                include: defaultInclude,
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+                include: defaultInclude,
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+                include: defaultInclude,
+            },
         ],
-        include: defaultInclude
-      },
-      {
-        test: /\.jsx?$/,
-        use: [{ loader: 'babel-loader' }],
-        include: defaultInclude
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
-      }
-    ]
-  },
-  target: 'electron-renderer',
-  plugins: [
-    new HtmlWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: 'bundle.css',
-      chunkFilename: '[id].css'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    // new MinifyPlugin()
-  ],
-  stats: {
-    colors: true,
-    children: false,
-    chunks: false,
-    modules: false
-  },
-  optimization: {
-    minimize: true
-  }
+    },
+    target: 'electron-renderer',
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'bundle.css',
+            chunkFilename: '[id].css',
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        // new MinifyPlugin()
+    ],
+    stats: {
+        colors: true,
+        children: false,
+        chunks: false,
+        modules: false,
+    },
+    optimization: {
+        minimize: true,
+    },
 }
